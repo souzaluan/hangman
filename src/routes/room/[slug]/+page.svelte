7a51bcfaac641eb69930e6835c20a26b85d0e970
Luan <luan.souza69@hotmail.com>
2024-08-05 21:23:17 -0300

    feat: creates new world modal

--- src/routes/room/[slug]/+page.svelte
+++ src/routes/room/[slug]/+page.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
 	import toast from 'svelte-french-toast';
-	import { IconArrowLeft, IconCopy, IconHeart, IconHeartFilled } from '@tabler/icons-svelte';
+	import {
+		IconArrowLeft,
+		IconCopy,
+		IconHeart,
+		IconHeartFilled,
+		IconInfoCircle
+	} from '@tabler/icons-svelte';
+	import Modal from '../../../components/modal.svelte';
 
 	export let data: { code: string };
 	const { code: roomCode } = data;
@@ -92,6 +99,25 @@
 		{/each}
 	</div>
 </section>
+
+<Modal isOpen>
+	<div class="new-word-container">
+		<h2 class="new-word-title">It's your turn!</h2>
+
+		<div class="input-container">
+			<div class="input-wrapper">
+				<input placeholder="Type a word" />
+			</div>
+
+			<div class="input-info">
+				<IconInfoCircle size="0.75rem" />
+				<span>Type a word for your opponent</span>
+			</div>
+		</div>
+
+		<button class="new-word-submit-button" on:click={() => goto('/room/ASAHB')}>Let's go</button>
+	</div>
+</Modal>
 
 <style>
 	.page-header {
@@ -197,5 +223,77 @@
 	}
 	.keyboard-container > button.right-attempt {
 		background-color: var(--color-brand-secondary);
+	}
+
+	.new-word-container {
+		width: 26rem;
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+
+		@media screen and (max-width: 540px) {
+			width: 20rem;
+		}
+	}
+
+	.new-word-title {
+		font-size: 1.75rem;
+		font-weight: 600;
+		color: var(--color-neutral-primary);
+	}
+
+	.input-container {
+		display: flex;
+		flex-direction: column;
+		gap: 0.5rem;
+	}
+	.input-wrapper {
+		height: 4rem;
+		display: flex;
+		align-items: center;
+		padding: 0.5rem;
+		border: 1px solid color-mix(in srgb, var(--color-neutral-primary) 35%, transparent);
+		border-radius: 0.5rem;
+	}
+	.input-wrapper > input {
+		width: 100%;
+		height: 100%;
+		font-size: 1.25rem;
+		font-weight: 600;
+		padding-left: 0.5rem;
+		color: var(--color-neutral-primary);
+		border: none;
+		border-radius: 0.25rem;
+		background-color: transparent;
+		outline: none;
+		text-transform: uppercase;
+	}
+	.input-wrapper > input::placeholder {
+		font-size: 1.25rem;
+		font-weight: 400;
+		opacity: 0.25;
+		text-transform: none;
+	}
+	.input-info {
+		display: flex;
+		align-items: center;
+		gap: 0.25rem;
+		font-size: 0.875rem;
+		color: color-mix(in srgb, var(--color-neutral-primary) 35%, transparent);
+	}
+	.new-word-submit-button {
+		min-height: 4rem;
+		max-height: 4rem;
+		flex: 1;
+		font-size: 1.5rem;
+		font-weight: 600;
+		border: none;
+		border-radius: 0.5rem;
+		background-color: var(--color-neutral-primary);
+		color: var(--color-neutral-secondary);
+	}
+	.new-word-submit-button:hover {
+		cursor: pointer;
+		filter: brightness(0.9);
 	}
 </style>
