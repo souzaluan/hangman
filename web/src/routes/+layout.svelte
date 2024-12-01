860650bf3ca756e8862943c350a894a5fe32f455
Luan Souza <76699000+souzaluan@users.noreply.github.com>
2024-12-01 10:33:16 -0300

    refactor: change share image url

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -14,14 +14,14 @@
 	<meta property="og:description" content="Multiplayer Hangman Game" />
 	<meta property="og:title" content="Hangman • Let's play" />
 	<meta property="og:type" content="website" />
-	<meta property="og:image" content="https://ibb.co/bLMZtkz" />
+	<meta property="og:image" content="https://i.postimg.cc/nLKL5fs2/364767910-554e7d6c-6cab-4727-ac1a-c72d0bd9e93e.png" />
 
 	<meta name="twitter:card" content="summary_large_image" />
 	<meta property="twitter:domain" content="hangman-alpha-gules.vercel.app" />
 	<meta property="twitter:url" content="https://hangman-alpha-gules.vercel.app/" />
 	<meta property="twitter:title" content="Hangman • Let's play" />
 	<meta name="twitter:description" content="Multiplayer Hangman Game" />
-	<meta name="twitter:image" content="https://ibb.co/bLMZtkz" />
+	<meta name="twitter:image" content="https://i.postimg.cc/nLKL5fs2/364767910-554e7d6c-6cab-4727-ac1a-c72d0bd9e93e.png" />
 </svelte:head>
 
 <main>
